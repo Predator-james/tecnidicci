875ccdd6da6ed9dbcb915cd44b7d3f7b8bc898bb
Técnicos en hacking Domina <73371736+Predator-james@users.noreply.github.com>
2020-12-02 23:52:03 -0600

    Create tecnidicci.sh

---
+++ tecnidicci.sh
@@ -0,0 +1,409 @@
+#!bin/bash
+clear
+echo .>>contraseñas.txt
+rm -rf contraseñas.txt
+echo -e '\e[1;34m█▀▀▀ █▀▀█ █▀▀█ █▀▀█ █▀▀█\e[0m'
+echo -e '\e[1;34m█▀▀▀ █▄▄▀ █▄▄▀ █░░█ █▄▄▀\e[0m'
+echo -e '\e[1;34m█▄▄▄ █░▀█ █░▀█ ▀▀▀▀ █░▀█\e[0m'
+echo -e '\e[1;34m|   TÉCNICOS EN HACKING     |\e[0m'
+echo -e '\e[1;34m|                           |\e[0m'
+echo -e '\e[1;34m|Diccionario de fuerza bruta|\e[0m'
+echo -e '\e[1;34m|                           |\e[0m'
+echo -e '\e[1;34m|_______By:JAMESJET_________|\e[0m'
+sleep 3
+echo -e '\e[1;31mINICIANDO\e[0m'
+sleep 4
+echo -e '\e[1;33mPrimer Nombre\e[0m'
+read PN
+sleep 2
+echo -e '\e[1;33mSegundo Nombre\e[0m'
+read SN
+sleep 2
+echo -e '\e[1;33mPrimer Apellido\e[0m'
+read PA
+sleep 2
+echo -e '\e[1;33mSegundo Apellido\e[0m'
+read SA
+sleep 2
+echo -e '\e[1;33mNombre de hijo o hija\e[0m'
+read Name
+sleep 2
+echo -e '\e[1;33mNombre de mascota\e[0m'
+read mt
+sleep 2
+echo $PN >>contraseñas.txt
+echo $PN 1 >>contraseñas.txt
+echo $PN 2 >>contraseñas.txt
+echo $PN 3 >>contraseñas.txt
+echo $PN 4 >>contraseñas.txt
+echo $PN 5 >>contraseñas.txt
+echo $PN 6 >>contraseñas.txt
+echo $PN 7 >>contraseñas.txt
+echo $PN 8 >>contraseñas.txt
+echo $PN 9 >>contraseñas.txt
+echo $PN 0 >>contraseñas.txt
+echo $PN 10 >>contraseñas.txt
+echo $PN 11 >>contraseñas.txt
+echo $PN 12 >>contraseñas.txt
+echo $PN 13 >>contraseñas.txt
+echo $PN 14 >>contraseñas.txt
+echo $PN 15 >>contraseñas.txt
+echo $PN 16 >>contraseñas.txt
+echo $PN 17 >>contraseñas.txt
+echo $PN 18 >>contraseñas.txt
+echo $PN 19 >>contraseñas.txt
+echo $PN 20 >>contraseñas.txt
+echo $PN 21 >>contraseñas.txt
+echo $PN 22 >>contraseñas.txt
+echo $PN 23 >>contraseñas.txt
+echo $PN 24 >>contraseñas.txt
+echo $PN 25 >>contraseñas.txt
+echo $PN 26 >>contraseñas.txt
+echo $PN 27 >>contraseñas.txt
+echo $PN 28 >>contraseñas.txt
+echo $PN 29 >>contraseñas.txt
+echo $PN 30 >>contraseñas.txt
+echo $PN 31 >>contraseñas.txt
+echo $PN 32 >>contraseñas.txt
+echo $PN 33  >>contraseñas.txt
+echo $PN 34 >>contraseñas.txt
+echo $PN 35 >>contraseñas.txt
+echo $PN 36 >>contraseñas.txt
+echo $PN 37 >>contraseñas.txt
+echo $PN 38 >>contraseñas.txt
+echo $PN 39 >>contraseñas.txt
+echo $PN 40 >>contraseñas.txt
+echo $PN 41 >>contraseñas.txt
+echo $PN 42 >>contraseñas.txt
+echo $PN 43 >>contraseñas.txt
+echo $PN 44 >>contraseñas.txt
+echo $PN 45 >>contraseñas.txt
+echo $PN 45 >>contraseñas.txt
+echo $PN 46 >>contraseñas.txt
+echo $PN 47 >>contraseñas.txt
+echo $PN 48 >>contraseñas.txt
+echo $PN 49 >>contraseñas.txt
+echo $PN 50 >>contraseñas.txt
+echo $PN 51 >>contraseñas.txt
+echo $PN 52 >>contraseñas.txt
+echo $PN 53 >>contraseñas.txt
+echo $PN 54 >>contraseñas.txt
+echo $PN 55 >>contraseñas.txt
+echo $PN 56 >>contraseñas.txt
+echo $PN 57 >>contraseñas.txt
+echo $PN 58 >>contraseñas.txt
+echo $PN 59 >>contraseñas.txt
+echo $PN 60 >>contraseñas.txt
+echo $PN $SN  >>contraseñas.txt
+echo $PN $SN 1 >>contraseñas.txt
+echo $PN $SN 2 >>contraseñas.txt
+echo $PN $SN 3 >>contraseñas.txt
+echo $PN $SN 4 >>contraseñas.txt
+echo $PN $SN 5 >>contraseñas.txt
+echo $PN $SN 6 >>contraseñas.txt
+echo $PN $SN 7 >>contraseñas.txt
+echo $PN $SN 8 >>contraseñas.txt
+echo $PN $SN 9 >>contraseñas.txt
+echo $PN $SN 0 >>contraseñas.txt
+echo $PN $SN 10 >>contraseñas.txt
+echo $PN $SN 11 >>contraseñas.txt
+echo $PN $SN 12 >>contraseñas.txt
+echo $PN $SN 13 >>contraseñas.txt
+echo $PN $SN 14 >>contraseñas.txt
+echo $PN $SN 15 >>contraseñas.txt
+echo $PN $SN 16 >>contraseñas.txt
+echo $PN $SN 17 >>contraseñas.txt
+echo $PN $SN 18 >>contraseñas.txt
+echo $PN $SN 19 >>contraseñas.txt
+echo $PN $SN 20 >>contraseñas.txt
+echo $PN $SN 21 >>contraseñas.txt
+echo $PN $SN 22 >>contraseñas.txt
+echo $PN $SN 23 >>contraseñas.txt
+echo $PN $SN 24 >>contraseñas.txt
+echo $PN $SN 25 >>contraseñas.txt
+echo $PN $SN 26 >>contraseñas.txt
+echo $PN $SN 27 >>contraseñas.txt
+echo $PN $SN 28 >>contraseñas.txt
+echo $PN $SN 29 >>contraseñas.txt
+echo $PN $SN 30 >>contraseñas.txt
+echo $PN $SN 31 >>contraseñas.txt
+echo $PN $SN 32 >>contraseñas.txt
+echo $PN $SN 33 >>contraseñas.txt
+echo $PN $SN 34 >>contraseñas.txt
+echo $PN $SN 35 >>contraseñas.txt
+echo $PN $SN 36 >>contraseñas.txt
+echo $PN $SN 37 >>contraseñas.txt
+echo $PN $SN 38 >>contraseñas.txt
+echo $PN $SN 39 >>contraseñas.txt
+echo $PN $SN 40 >>contraseñas.txt
+echo $PN $SN 41 >>contraseñas.txt
+echo $PN $SN 42 >>contraseñas.txt
+echo $PN $SN 43 >>contraseñas.txt
+echo $PN $SN 44 >>contraseñas.txt
+echo $PN $SN 45 >>contraseñas.txt
+echo $PN $SN 45 >>contraseñas.txt
+echo $PN $SN 46 >>contraseñas.txt
+echo $PN $SN 47 >>contraseñas.txt
+echo $PN $SN 48 >>contraseñas.txt
+echo $PN $SN 49 >>contraseñas.txt
+echo $PN $SN 50 >>contraseñas.txt
+echo $PN $SN 51 >>contraseñas.txt
+echo $PN $SN 52 >>contraseñas.txt
+echo $PN $SN 53 >>contraseñas.txt
+echo $PN $SN 54 >>contraseñas.txt
+echo $PN $SN 55 >>contraseñas.txt
+echo $PN $SN 56 >>contraseñas.txt
+echo $PN $SN 57 >>contraseñas.txt
+echo $PN $SN 58 >>contraseñas.txt
+echo $PN $SN 59 >>contraseñas.txt
+echo $PN $SN 60 >>contraseñas.txt
+echo $PN $SN $PA >>contraseñas.txt
+echo $PN $SN $PA 1 >>contraseñas.txt
+echo $PN $SN $PA 2 >>contraseñas.txt
+echo $PN $SN $PA 3 >>contraseñas.txt
+echo $PN $SN $PA 4 >>contraseñas.txt
+echo $PN $SN $PA 5 >>contraseñas.txt
+echo $PN $SN $PA 6 >>contraseñas.txt
+echo $PN $SN $PA 7 >>contraseñas.txt
+echo $PN $SN $PA 8 >>contraseñas.txt
+echo $PN $SN $PA 9 >>contraseñas.txt
+echo $PN $SN $PA 0 >>contraseñas.txt
+echo $PN $SN $PA 10 >>contraseñas.txt
+echo $PN $SN $PA 11 >>contraseñas.txt
+echo $PN $SN $PA 12 >>contraseñas.txt
+echo $PN $SN $PA 13 >>contraseñas.txt
+echo $PN $SN $PA 14 >>contraseñas.txt
+echo $PN $SN $PA 15 >>contraseñas.txt
+echo $PN $SN $PA 16 >>contraseñas.txt
+echo $PN $SN $PA 17 >>contraseñas.txt
+echo $PN $SN $PA 18 >>contraseñas.txt
+echo $PN $SN $PA 19 >>contraseñas.txt
+echo $PN $SN $PA 20 >>contraseñas.txt
+echo $PN $SN $PA 21 >>contraseñas.txt
+echo $PN $SN $PA 22 >>contraseñas.txt
+echo $PN $SN $PA 23 >>contraseñas.txt
+echo $PN $SN $PA 24 >>contraseñas.txt
+echo $PN $SN $PA 25 >>contraseñas.txt
+echo $PN $SN $PA 26 >>contraseñas.txt
+echo $PN $SN $PA 27 >>contraseñas.txt
+echo $PN $SN $PA 28 >>contraseñas.txt
+echo $PN $SN $PA 29 >>contraseñas.txt
+echo $PN $SN $PA 30 >>contraseñas.txt
+echo $PN $SN $PA 31 >>contraseñas.txt
+echo $PN $SN $PA 32 >>contraseñas.txt
+echo $PN $SN $PA 33 >>contraseñas.txt
+echo $PN $SN $PA 34 >>contraseñas.txt
+echo $PN $SN $PA 35 >>contraseñas.txt
+echo $PN $SN $PA 36 >>contraseñas.txt
+echo $PN $SN $PA 37 >>contraseñas.txt
+echo $PN $SN $PA 38 >>contraseñas.txt
+echo $PN $SN $PA 39 >>contraseñas.txt
+echo $PN $SN $PA 40 >>contraseñas.txt
+echo $PN $SN $PA 41 >>contraseñas.txt
+echo $PN $SN $PA 42 >>contraseñas.txt
+echo $PN $SN $PA 43 >>contraseñas.txt
+echo $PN $SN $PA 44 >>contraseñas.txt
+echo $PN $SN $PA 45 >>contraseñas.txt
+echo $PN $SN $PA 45 >>contraseñas.txt
+echo $PN $SN $PA 46 >>contraseñas.txt
+echo $PN $SN $PA 47 >>contraseñas.txt
+echo $PN $SN $PA 48 >>contraseñas.txt
+echo $PN $SN $PA 49 >>contraseñas.txt
+echo $PN $SN $PA 50 >>contraseñas.txt
+echo $PN $SN $PA 51 >>contraseñas.txt
+echo $PN $SN $PA 52 >>contraseñas.txt
+echo $PN $SN $PA 53 >>contraseñas.txt
+echo $PN $SN $PA 54 >>contraseñas.txt
+echo $PN $SN $PA 55 >>contraseñas.txt
+echo $PN $SN $PA 56 >>contraseñas.txt
+echo $PN $SN $PA 57 >>contraseñas.txt
+echo $PN $SN $PA 58 >>contraseñas.txt
+echo $PN $SN $PA 59 >>contraseñas.txt
+echo $PN $SN $PA 60 >>contraseñas.txt
+echo $PN $SN $PA $SA 1 >>contraseñas.txt
+echo $PN $SN $PA $SA 2 >>contraseñas.txt
+echo $PN $SN $PA $SA 3 >>contraseñas.txt
+echo $PN $SN $PA $SA 4 >>contraseñas.txt
+echo $PN $SN $PA $SA 5 >>contraseñas.txt
+echo $PN $SN $PA $SA 6 >>contraseñas.txt
+echo $PN $SN $PA $SA 7 >>contraseñas.txt
+echo $PN $SN $PA $SA 8 >>contraseñas.txt
+echo $PN $SN $PA $SA 9 >>contraseñas.txt
+echo $PN $SN $PA $SA 0 >>contraseñas.txt
+echo $PN $SN $PA $SA 10 >>contraseñas.txt
+echo $PN $SN $PA $SA 11 >>contraseñas.txt
+echo $PN $SN $PA $SA 12 >>contraseñas.txt
+echo $PN $SN $PA $SA 13 >>contraseñas.txt
+echo $PN $SN $PA $SA 14 >>contraseñas.txt
+echo $PN $SN $PA $SA 15 >>contraseñas.txt
+echo $PN $SN $PA $SA 16 >>contraseñas.txt
+echo $PN $SN $PA $SA 17 >>contraseñas.txt
+echo $PN $SN $PA $SA 18 >>contraseñas.txt
+echo $PN $SN $PA $SA 19 >>contraseñas.txt
+echo $PN $SN $PA $SA 20 >>contraseñas.txt
+echo $PN $SN $PA $SA 21 >>contraseñas.txt
+echo $PN $SN $PA $SA 22 >>contraseñas.txt
+echo $PN $SN $PA $SA 23 >>contraseñas.txt
+echo $PN $SN $PA $SA 24 >>contraseñas.txt
+echo $PN $SN $PA $SA 25 >>contraseñas.txt
+echo $PN $SN $PA $SA 26 >>contraseñas.txt
+echo $PN $SN $PA $SA 27 >>contraseñas.txt
+echo $PN $SN $PA $SA 28 >>contraseñas.txt
+echo $PN $SN $PA $SA 29 >>contraseñas.txt
+echo $PN $SN $PA $SA 30 >>contraseñas.txt
+echo $PN $SN $PA $SA 31 >>contraseñas.txt
+echo $PN $SN $PA $SA 32 >>contraseñas.txt
+echo $PN $SN $PA $SA 33 >>contraseñas.txt
+echo $PN $SN $PA $SA 34 >>contraseñas.txt
+echo $PN $SN $PA $SA 35 >>contraseñas.txt
+echo $PN $SN $PA $SA 36 >>contraseñas.txt
+echo $PN $SN $PA $SA 37 >>contraseñas.txt
+echo $PN $SN $PA $SA 38 >>contraseñas.txt
+echo $PN $SN $PA $SA 39 >>contraseñas.txt
+echo $PN $SN $PA $SA 40 >>contraseñas.txt
+echo $PN $SN $PA $SA 41 >>contraseñas.txt
+echo $PN $SN $PA $SA 42 >>contraseñas.txt
+echo $PN $SN $PA $SA 43 >>contraseñas.txt
+echo $PN $SN $PA $SA 44 >>contraseñas.txt
+echo $PN $SN $PA $SA 45 >>contraseñas.txt
+echo $PN $SN $PA $SA 46 >>contraseñas.txt
+echo $PN $SN $PA $SA 47 >>contraseñas.txt
+echo $PN $SN $PA $SA 48 >>contraseñas.txt
+echo $PN $SN $PA $SA 49 >>contraseñas.txt
+echo $PN $SN $PA $SA 50 >>contraseñas.txt
+echo $PN $SN $PA $SA 51 >>contraseñas.txt
+echo $PN $SN $PA $SA 52 >>contraseñas.txt
+echo $PN $SN $PA $SA 53 >>contraseñas.txt
+echo $PN $SN $PA $SA 54 >>contraseñas.txt
+echo $PN $SN $PA $SA 55 >>contraseñas.txt
+echo $PN $SN $PA $SA 56 >>contraseñas.txt
+echo $PN $SN $PA $SA 57 >>contraseñas.txt
+echo $PN $SN $PA $SA 58 >>contraseñas.txt
+echo $PN $SN $PA $SA 59 >>contraseñas.txt
+echo $PN $SN $PA $SA 60 >>contraseñas.txt
+echo $Name >>contraseñas.txt
+echo $Name 1 >>contraseñas.txt
+echo $Name 2 >>contraseñas.txt
+echo $Name 3 >>contraseñas.txt
+echo $Name 4 >>contraseñas.txt
+echo $Name 5 >>contraseñas.txt
+echo $Name 6 >>contraseñas.txt
+echo $Name 7 >>contraseñas.txt
+echo $Name 8 >>contraseñas.txt
+echo $Name 9 >>contraseñas.txt
+echo $Name 0 >>contraseñas.txt
+echo $Name 10 >>contraseñas.txt
+echo $Name 11 >>contraseñas.txt
+echo $Name 12 >>contraseñas.txt
+echo $Name 13 >>contraseñas.txt
+echo $Name 14 >>contraseñas.txt
+echo $Name 15 >>contraseñas.txt
+echo $Name 16 >>contraseñas.txt
+echo $Name 17 >>contraseñas.txt
+echo $Name 18 >>contraseñas.txt
+echo $Name 19 >>contraseñas.txt
+echo $Name 20 >>contraseñas.txt
+echo $Name 21 >>contraseñas.txt
+echo $Name 22 >>contraseñas.txt
+echo $Name 23 >>contraseñas.txt
+echo $Name 24 >>contraseñas.txt
+echo $Name 25 >>contraseñas.txt
+echo $Name 26 >>contraseñas.txt
+echo $Name 27 >>contraseñas.txt
+echo $Name 28 >>contraseñas.txt
+echo $Name 29 >>contraseñas.txt
+echo $Name 30 >>contraseñas.txt
+echo $Name 31 >>contraseñas.txt
+echo $Name 32 >>contraseñas.txt
+echo $Name 33  >>contraseñas.txt
+echo $Name 34 >>contraseñas.txt
+echo $Name 35 >>contraseñas.txt
+echo $Name 36 >>contraseñas.txt
+echo $Name 37 >>contraseñas.txt
+echo $Name 38 >>contraseñas.txt
+echo $Name 39 >>contraseñas.txt
+echo $Name 40 >>contraseñas.txt
+echo $Name 41 >>contraseñas.txt
+echo $Name 42 >>contraseñas.txt
+echo $Name 43 >>contraseñas.txt
+echo $Name 44 >>contraseñas.txt
+echo $Name 45 >>contraseñas.txt
+echo $Name 45 >>contraseñas.txt
+echo $Name 46 >>contraseñas.txt
+echo $Name 47 >>contraseñas.txt
+echo $Name 48 >>contraseñas.txt
+echo $Name 49 >>contraseñas.txt
+echo $Name 50 >>contraseñas.txt
+echo $Name 51 >>contraseñas.txt
+echo $Name 52 >>contraseñas.txt
+echo $Name 53 >>contraseñas.txt
+echo $Name 54 >>contraseñas.txt
+echo $Name 55 >>contraseñas.txt
+echo $Name 56 >>contraseñas.txt
+echo $Name 57 >>contraseñas.txt
+echo $Name 58 >>contraseñas.txt
+echo $Name 59 >>contraseñas.txt
+echo $Name 60 >>contraseñas.txt
+echo $mt >>contraseñas.txt
+echo $mt 1 >>contraseñas.txt
+echo $mt 2 >>contraseñas.txt
+echo $mt 3 >>contraseñas.txt
+echo $mt 4 >>contraseñas.txt
+echo $mt 5 >>contraseñas.txt
+echo $mt 6 >>contraseñas.txt
+echo $mt 7 >>contraseñas.txt
+echo $mt 8 >>contraseñas.txt
+echo $mt 9 >>contraseñas.txt
+echo $mt 0 >>contraseñas.txt
+echo $mt 10 >>contraseñas.txt
+echo $mt 11 >>contraseñas.txt
+echo $mt 12 >>contraseñas.txt
+echo $mt 13 >>contraseñas.txt
+echo $mt 14 >>contraseñas.txt
+echo $mt 15 >>contraseñas.txt
+echo $mt 16 >>contraseñas.txt
+echo $mt 17 >>contraseñas.txt
+echo $mt 18 >>contraseñas.txt
+echo $mt 19 >>contraseñas.txt
+echo $mt 20 >>contraseñas.txt
+echo $mt 21 >>contraseñas.txt
+echo $mt 22 >>contraseñas.txt
+echo $mt 23 >>contraseñas.txt
+echo $mt 24 >>contraseñas.txt
+echo $mt 25 >>contraseñas.txt
+echo $mt 26 >>contraseñas.txt
+echo $mt 27 >>contraseñas.txt
+echo $mt 28 >>contraseñas.txt
+echo $mt 29 >>contraseñas.txt
+echo $mt 30 >>contraseñas.txt
+echo $mt 31 >>contraseñas.txt
+echo $mt 32 >>contraseñas.txt
+echo $mt 33  >>contraseñas.txt
+echo $mt 34 >>contraseñas.txt
+echo $mt 35 >>contraseñas.txt
+echo $mt 36 >>contraseñas.txt
+echo $mt 37 >>contraseñas.txt
+echo $mt 38 >>contraseñas.txt
+echo $mt 39 >>contraseñas.txt
+echo $mt 40 >>contraseñas.txt
+echo $mt 41 >>contraseñas.txt
+echo $mt 42 >>contraseñas.txt
+echo $mt 43 >>contraseñas.txt
+echo $mt 44 >>contraseñas.txt
+echo $mt 45 >>contraseñas.txt
+echo $mt 45 >>contraseñas.txt
+echo $mt 46 >>contraseñas.txt
+echo $mt 47 >>contraseñas.txt
+echo $mt 48 >>contraseñas.txt
+echo $mt 49 >>contraseñas.txt
+echo $mt 50 >>contraseñas.txt
+echo $mt 51 >>contraseñas.txt
+echo $mt 52 >>contraseñas.txt
+echo $mt 53 >>contraseñas.txt
+echo $mt 54 >>contraseñas.txt
+echo $mt 55 >>contraseñas.txt
+echo $mt 56 >>contraseñas.txt
+echo $mt 57 >>contraseñas.txt
+echo $mt 58 >>contraseñas.txt
+echo $mt 59 >>contraseñas.txt
+echo $mt 60 >>contraseñas.txt
